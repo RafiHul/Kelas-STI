b58d07e7dc5e0aa349c1bb8e1f6c6c25ae134732
Rafihul <rafihul2905@gmail.com>
2024-12-12 14:07:15 +0700

    feat: login register

--- app/src/main/java/com/stiproject/kelassti/fragment/LoginFragment.kt
+++ app/src/main/java/com/stiproject/kelassti/fragment/LoginFragment.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.stiproject.kelassti.R
 import com.stiproject.kelassti.databinding.FragmentLoginBinding
+import com.stiproject.kelassti.model.request.LoginRequest
 import com.stiproject.kelassti.viewmodel.UserViewModel
 import kotlin.getValue
 
@@ -18,6 +22,7 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
     private val binding get() = _binding!!
 
     val userViewModel: UserViewModel by activityViewModels()
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +30,28 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
     ): View {
         _binding = FragmentLoginBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = findNavController()
+
+        binding.buttonToRegister.setOnClickListener{
+            navController.navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+
+        binding.buttonLogin.setOnClickListener{
+            val username = binding.editTextTextUsernameLogin.text.toString()
+            val password = binding.editTextTextPasswordLogin.text.toString()
+
+            if (username.isNotEmpty() && password.isNotEmpty()){
+                userViewModel.userLogin(
+                    LoginRequest(username,password)
+                ){
+                    Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/stiproject/kelassti/fragment/RegisterFragment.kt
+++ app/src/main/java/com/stiproject/kelassti/fragment/RegisterFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.stiproject.kelassti.R
 import com.stiproject.kelassti.databinding.FragmentRegisterBinding
+import com.stiproject.kelassti.model.request.RegisterRequest
 import com.stiproject.kelassti.viewmodel.UserViewModel
 import kotlin.getValue
 
@@ -17,6 +21,7 @@ class RegisterFragment : Fragment(R.layout.fragment_register) {
     private val binding get() = _binding!!
 
     val userViewModel: UserViewModel by activityViewModels()
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +29,26 @@ class RegisterFragment : Fragment(R.layout.fragment_register) {
     ): View {
         _binding = FragmentRegisterBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = findNavController()
+
+        binding.buttonToLogin.setOnClickListener{
+            navController.navigate(R.id.action_registerFragment_to_loginFragment)
+        }
+
+        binding.buttonRegister.setOnClickListener{
+            val name = binding.editTextTextNameRegister.text.toString()
+            val username = binding.editTextTextPasswordRegister.text.toString()
+            val password = binding.editTextTextPasswordRegister.text.toString()
+            userViewModel.userRegister(
+                RegisterRequest(username,password,name)
+            ){
+                Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onDestroyView() {
