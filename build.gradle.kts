357fbd7aeb09259c3e84449c4e414e738e4d9258
Rafihul <rafihul2905@gmail.com>
2024-12-12 13:04:12 +0700

    gradle: update some dependency version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        val nav_version = "2.7.7"
+        val nav_version = "2.8.4"
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
     }
 }
@@ -13,5 +13,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     id("com.google.dagger.hilt.android") version "2.52" apply false
-    id("com.google.devtools.ksp") version "2.0.21-1.0.25" apply false
+    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
 }
