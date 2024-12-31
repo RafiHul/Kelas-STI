234957ef4535e2dc5f9822b1d8e5ede7d06afdc0
Rafihul <rafihul2905@gmail.com>
2024-12-31 10:46:28 +0700

    ui: profile fragment

---
+++ app/src/main/java/com/stiproject/kelassti/fragment/ProfileFragment.kt
@@ -0,0 +1,29 @@
+package com.stiproject.kelassti.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.stiproject.kelassti.R
+import com.stiproject.kelassti.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get () = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
