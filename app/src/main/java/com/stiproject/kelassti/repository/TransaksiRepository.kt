95b2b1c115ccc67303e23ec741d17a32df802266
Rafihul <rafihul2905@gmail.com>
2025-01-13 09:26:39 +0700

    api: updatetransaksibyid

--- app/src/main/java/com/stiproject/kelassti/repository/TransaksiRepository.kt
+++ app/src/main/java/com/stiproject/kelassti/repository/TransaksiRepository.kt
@@ -1,6 +1,6 @@
 package com.stiproject.kelassti.repository
 
-import com.stiproject.kelassti.model.request.AddKasRequest
+import com.stiproject.kelassti.model.request.KasRequest
 import com.stiproject.kelassti.model.response.transaksi.TransaksiDataByIdResponse
 import com.stiproject.kelassti.model.response.transaksi.TransaksiDataResponse
 import com.stiproject.kelassti.retrofit.RetrofitInstance
@@ -12,15 +12,19 @@ import javax.inject.Singleton
 class TransaksiRepository @Inject constructor() {
     val transaksiService = RetrofitInstance.getTransaksiService
 
-    suspend fun getTransaksi(): Response<TransaksiDataResponse> {
+    suspend fun getAllTransaksi(): Response<TransaksiDataResponse> {
         return transaksiService.getTransaksi()
     }
 
-    suspend fun postTransaksi(jwtToken: String, addKasRequest: AddKasRequest): Response<TransaksiDataByIdResponse> {
-        return transaksiService.postTransaksi(jwtToken,addKasRequest)
+    suspend fun postTransaksi(jwtToken: String, kasRequest: KasRequest): Response<TransaksiDataByIdResponse> {
+        return transaksiService.postTransaksi(jwtToken,kasRequest)
     }
 
     suspend fun getTransaksiById(id: String): Response<TransaksiDataByIdResponse> {
         return transaksiService.getTransaksiById(id)
+    }
+
+    suspend fun updateTransaksiById(id: String, jwtToken: String, kasRequest: KasRequest): Response<TransaksiDataByIdResponse> {
+        return transaksiService.updateTransaksiById(id,jwtToken,kasRequest)
     }
 }
--- app/src/main/java/com/stiproject/kelassti/retrofit/TransaksiApi.kt
+++ app/src/main/java/com/stiproject/kelassti/retrofit/TransaksiApi.kt
@@ -1,6 +1,6 @@
 package com.stiproject.kelassti.retrofit
 
-import com.stiproject.kelassti.model.request.AddKasRequest
+import com.stiproject.kelassti.model.request.KasRequest
 import com.stiproject.kelassti.model.response.transaksi.TransaksiDataByIdResponse
 import com.stiproject.kelassti.model.response.transaksi.TransaksiDataResponse
 import retrofit2.Response
@@ -8,6 +8,7 @@ import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface TransaksiApi {
@@ -17,11 +18,18 @@ interface TransaksiApi {
     @POST("/transaksi")
     suspend fun postTransaksi(
         @Header("Authorization") jwt: String,
-        @Body addKasRequest: AddKasRequest
+        @Body kasRequest: KasRequest
     ): Response<TransaksiDataByIdResponse>
 
     @GET("/transaksi/{id}")
     suspend fun getTransaksiById(
         @Path("id") id: String
+    ): Response<TransaksiDataByIdResponse>
+
+    @PUT("/transaksi/{id}")
+    suspend fun updateTransaksiById(
+        @Path("id") id: String,
+        @Header("Authorization") jwt: String,
+        @Body kasRequest: KasRequest
     ): Response<TransaksiDataByIdResponse>
 }
