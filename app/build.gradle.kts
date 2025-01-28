6fd672fbc3633293513433b86d2cceeb76bcffdd
Rafihul <rafihul2905@gmail.com>
2025-01-28 12:51:40 +0700

    gradle: update some dependency and add workmanager dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,27 +53,29 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.4")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.5")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-livedata-core-ktx:2.8.7")
 
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.1.0")
-    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.google.code.gson:gson:2.11.0")
 
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
 
-    implementation("androidx.datastore:datastore-preferences:1.1.1")
-    implementation("androidx.datastore:datastore-preferences-core:1.1.1")
+    implementation("androidx.datastore:datastore-preferences:1.1.2")
+    implementation("androidx.datastore:datastore-preferences-core:1.1.2")
 
-    implementation("com.google.dagger:hilt-android:2.52")
-    ksp("com.google.dagger:hilt-android-compiler:2.52")
+    implementation("com.google.dagger:hilt-android:2.55")
+    ksp("com.google.dagger:hilt-android-compiler:2.55")
 
     implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("com.github.ibrahimsn98:SmoothBottomBar:1.7.9")
-    implementation("androidx.paging:paging-runtime:3.3.2")
+    implementation("androidx.paging:paging-runtime:3.3.5")
+
+    implementation("androidx.work:work-runtime-ktx:2.9.1")
 }
