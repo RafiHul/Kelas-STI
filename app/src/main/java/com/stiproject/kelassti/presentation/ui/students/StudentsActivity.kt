cd82f45403a75cf912289938bacfece58d9e990a
Rafihul <rafihul2905@gmail.com>
2025-04-22 09:17:32 +0700

    ui: students fragment & activity

---
+++ app/src/main/java/com/stiproject/kelassti/presentation/ui/students/StudentsActivity.kt
@@ -0,0 +1,21 @@
+package com.stiproject.kelassti.presentation.ui.students
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.stiproject.kelassti.R
+
+class StudentsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_students)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
